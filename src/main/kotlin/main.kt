fab57d2b7272a542eff7cd25d23e335f283c0dab
Estefano22 <sandulescustefano8@gmail.com>
2021-04-15 14:18:28 +0200

    Hecho bien

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,58 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.*
+
 
 fun main(args: Array<String>) {
-    val listjugador1 = listOf<Int>(1,2,3,4,5,6)
-    val listjugador2 = listOf<Int>(1,2,3,4,5,6)
+
+    val random =Random()
+    var numsacado = 0
+
+    val listjugador1 = mutableListOf<Int>()
+    val listjugador2 = mutableListOf<Int>()
 
     var resultadojugador1 = 0
     var resultadojugador2 = 0
 
+
+println("EMPEZAMOS")
+    println("3")
+    println("2")
+    println("1")
+    println("GO!")
     runBlocking {
         this.launch {
             repeat(10) {
-                println("Jugador 1 a tirado y ha sacado: ${listjugador1.random()}")
+               // numsacado = random.nextInt(6)+1
+                numsacado = (1..6).random()
+                println("Jugador 1 a tirado y ha sacado: $numsacado")
+                listjugador1.add(numsacado)
                 delay(1000)
+                resultadojugador1 += numsacado
 
-                 resultadojugador1 += listjugador1.random()
             }
-            println("Jugador 1 ha terminado tirar")
+
+            println("resultado de las tiradas del Primer Jugador: $listjugador1")
+            println("Jugador 1 ha terminado de tirar")
         }
 
         this.launch {
             repeat(10) {
-                println("Jugador 2 a tirado y ha sacado: ${listjugador2.random()}")
+                //numsacado = random.nextInt(6)+1
+                numsacado = (1..6).random()
+                println("Jugador 2 a tirado y ha sacado:$numsacado")
+                listjugador2.add(numsacado)
                 delay(1500)
-                resultadojugador2 += listjugador2.random()
+                resultadojugador2 += numsacado
+
             }
-            println("Jugador 2 ha terminado tirar")
+            println("resultado de las tiradas del Segundo Jugador: $listjugador2")
+            println("Jugador 2 ha terminado de tirar")
         }
     }
 
     println("El resultado del jugador 1 es:$resultadojugador1")
     println("El resultado del jugador 2 es:$resultadojugador2")
+
 }
